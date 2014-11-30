fedbdc1b2b50923f1a0605b379f2af7ede1af69f
Ariel Mashraki <ariel.mashraki@ironsrc.com>
2014-11-30 14:57:36 +0200

    refac(example): deprcated files

--- example/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var DoQumentDB = require('..');
-var Promise    = require('bluebird');
-var DB_CONFIG  = require('../config'); //add your config
-
-DoQumentDB.createConnection(DB_CONFIG.HOST, DB_CONFIG.OPTIONS);
-
-//...
