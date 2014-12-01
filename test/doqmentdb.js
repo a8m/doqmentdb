62d4b030b560cdbc1ecb09aec31ec8d483c18984
Ariel Mashraki <ariel.mashraki@ironsrc.com>
2014-12-01 08:25:16 +0200

    test(.find): test db.find behavior

--- test/doqmentdb.js
+++ test/doqmentdb.js
@@ -1,4 +1,5 @@
 'use strict';
+/*global beforeEach, afterEach*/
 var DocumentDB = require('documentdb').DocumentClient;
 var DoQmentDB  = require('..');
 var _          = require('agile');
@@ -6,15 +7,23 @@ var sinon      = require('sinon');
 var should     = require('should');
 var Promise    = require('bluebird');
 
-var toArray = function(args) {
+
+// Helpers
+var DB_MOCK = { _self: '/self', _colls: '/colls'};
+function toArray(args) {
   return { toArray: function(fb) { fb.apply(null, args);} }
-};
+}
+
+function assertCalled(q, done, toCalled) {
+  q.then(function(res) {
+    (toCalled.called).should.eql(true);
+    done();
+  });
+}
+
 
 describe('DoqmentDB', function() {
   describe('DatabaseManager', function() {
-    beforeEach(function() {
-
-    });
 
     describe('creating `new` DatabaseManager', function() {
       it('should get a not DocumentDB instance(connection) and throw', function() {
@@ -69,6 +78,41 @@ describe('DoqmentDB', function() {
     });
 
     // Public api, .find(), .create(), ...
+    describe('Public API', function() {
+      var connection = new DocumentDB('host', { masterKey: 'key' });
+      var dbManager  = new DoQmentDB(connection, '');
+      var queryStub, readStub;
 
+      beforeEach(function() {
+        var stub = sinon.stub(DocumentDB.prototype,  'queryDatabases');
+        queryStub = sinon.stub(DocumentDB.prototype, 'queryCollections');
+        readStub = sinon.stub(DocumentDB.prototype,  'readCollections');
+        stub.returns(toArray([null, DB_MOCK]));
+        queryStub.returns(toArray([null, [1, 2]]));
+        readStub.returns(toArray([null, [1, 2]]));
+      });
+
+      describe('.find()', function() {
+        it('should get object properties and call `queryCollections`', function(done) {
+          assertCalled(dbManager.find({ id: 'name' }), done, queryStub);
+        });
+
+        //TODO: calledWith assertion
+
+        it('should get an empty object and call `readCollections`', function(done) {
+          assertCalled(dbManager.find({}), done, readStub);
+        });
+
+        it('should get an `undefined` params and call `readCollections`', function(done) {
+          assertCalled(dbManager.find(undefined), done, readStub);
+        });
+      });
+
+      afterEach(function() {
+        queryStub.restore();
+        readStub.restore();
+        DocumentDB.prototype.queryDatabases.restore();
+      });
+    });
   });
 });
