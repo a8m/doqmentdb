e822194f3c17d63d3b7b5efd89fedfa6b49a6bb0
Ariel Mashraki <ariel.mashraki@ironsrc.com>
2014-12-31 10:51:51 +0200

    docs(example): create ODM example

--- example/index.js
+++ example/index.js
@@ -5,13 +5,6 @@ var connection = new (require('documentdb').DocumentClient)(CONFIG.HOST, CONFIG.
 
 var db = new DoQmentDB(connection, CONFIG.DB);
 var users = db.use('users');
-users.schema({
-  name: {
-    type: String,
-    regex: /^[a-zA-Z0-9|/d]{3,}$/,
-    expose: true
-  }
-});
 
 users.find({})
   .then(console.log);
---
+++ example/odm/index.js
@@ -0,0 +1,47 @@
+'use strict';
+var DoQmentDB  = require('../..');
+var CONFIG     = require('../../config');
+var model      = require('./model');
+var connection = new (require('documentdb').DocumentClient)(CONFIG.HOST, CONFIG.OPTIONS);
+
+var db = new DoQmentDB(connection, CONFIG.DB);
+var users = db.use('users');
+users.schema(model);
+
+users.create({ password: 'Ar2!as_s'})
+  .then(console.log)
+  .catch(console.log);
+  /*
+   [Error:
+   `name` must be type string, and least 3 chars,
+   `email` must be type string, valid email address, and least 10 chars
+   ]
+   */
+
+users.create({ name: 'Ariel', email: 'ariel.com', password: 'Ar2!as_s'})
+  .then(console.log)
+  .catch(console.log);
+/*
+ [Error:
+ `email` must be type string, valid email address, and least 10 chars
+ ]
+ */
+
+users.create({ name: 'Ariel', email: 'a8m@gm.com', password: 'Ar2!as_s'})
+  .then(console.log)
+  .catch(console.log);
+/*
+ {
+   name: 'Ariel',
+   email: 'a8m@gm.com',
+   password: 'Ar2!as_s',
+   id: '2eb7...c0',
+    ...
+ }
+ */
+
+users.find({})
+  .then(console.log);
+/*
+ Get all documents but without exposing fields(i.e: omit `password` field)
+ */
