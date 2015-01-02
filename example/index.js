a376970080eb9461d46ebe71b1a3a672f09934b7
Ariel Mashraki <ariel.mashraki@ironsrc.com>
2015-01-03 01:50:13 +0200

    example(middleware): add example

--- example/index.js
+++ example/index.js
@@ -6,5 +6,29 @@ var connection = new (require('documentdb').DocumentClient)(CONFIG.HOST, CONFIG.
 var db = new DoQmentDB(connection, CONFIG.DB);
 var users = db.use('users');
 
-users.find({})
+// The order is important
+// create/save its the same thing
+users.pre('save', function(next) {
+  var doc = this;
+  console.log('doc', 1);
+  next();
+}, function(next) {
+  // some async thing
+  console.log(2);
+  setTimeout(next, 3000);
+});
+
+// one more..
+users.pre('create', function(next) {
+  console.log(3);
+  next();
+});
+
+users.post('save', function(doc) {
+  console.log(doc._self);
+}, function(doc) {
+  // ...
+});
+
+users.create({ name: 'Ariel', age: 26 })
   .then(console.log);
